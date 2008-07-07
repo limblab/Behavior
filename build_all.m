b1920b6b95ec84e50c041a54b9064c84f8948a2c
Brian London <brianmlondon@gmail.com>
2008-07-07 22:08:00 +0000

    build scripts working
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@133 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

--- build_all.m
+++ build_all.m
@@ -1,91 +1,14 @@
 % calls mex on all c files
 % be careful, dependencies are not working yet
 
-tic;
-disp('Build Started')
-
-% s-functions
-cfiles = { ...
-    'Byte2Bits',    ...
-    'serPos',       ...
-    'mastercon_bs', ...
-    'mastercon_co', ...
-    'mastercon_fc', ...
-    'mastercon_rw', ...
-    'mastercon_mg', ...
-    'mastercon_wf'  ...
+T = {...
+    {'random_walk.dlm', 'mastercon_rw.mexw32', 'serPos.mexw32', 'Byte2Bits.mexw32'}, ...
+    {'center_out.dlm', 'mastercon_co.mexw32', 'serPos.mexw32', 'Byte2Bits.mexw32'}, ...
+    {'forced_choice.dlm', 'mastercon_fc.mexw32', 'serPos.mexw32', 'Byte2Bits.mexw32'}, ...
+    {'bump_stim.dlm', 'mastercon_bs.mexw32', 'serPos.mexw32', 'Byte2Bits.mexw32'}, ...
+    {'multi_gadget.dlm', 'mastercon_mg.mexw32', 'serPos.mexw32', 'Byte2Bits.mexw32'}, ...
+    {'wrist_flexion.dlm', 'mastercon_wf.mexw32', 'serPos.mexw32', 'Byte2Bits.mexw32'} ...
 };
 
-% RTW models
-rtwfiles = {         ...
-    'bump_stim',     ...
-    'center_out',    ...
-    'forced_choice', ...
-    'random_walk',   ...
-    'multi_gadget',  ...
-    'wrist_flexion'  ...
-};
-
-% build s-functions
-for i = 1:length(cfiles)
-    build = 0;
-    
-    filename = strcat(cfiles(i), '.c');
-    filename = filename{1};
-    filedate = dir(filename);
-    filedate = filedate.datenum;
-    
-    compname = strcat(cfiles(i), '.mexw32');
-    compname = compname{1};
-    compdate = dir(compname);
-    if (~isempty(compdate))
-        compdate = compdate.datenum;
-        if (filedate > compdate)
-            build = 1;
-        end 
-    else 
-        build = 1;
-    end
-    
-    if (build)
-        disp(['building: ' filename]);
-        mex(filename);
-    else
-        disp(['skipped:  ' filename]);
-    end
-end
-
-% build models
-for i = 1:length(rtwfiles)
-    build = 0;
-    
-    filename = strcat(rtwfiles(i), '.mdl');
-    filename = filename{1};
-    filedate = dir(filename);
-    filedate = filedate.datenum;
-    
-    % Check if mdl file has been updated
-    compname = strcat(rtwfiles(i), '.dlm');
-    compname = compname{1};
-    compdate = dir(compname);
-    if (~isempty(compdate))
-        compdate = compdate.datenum;
-        if (filedate > compdate)
-            build = 1;
-        end 
-    else 
-        build = 1;
-    end
-        
-    if (build)
-        disp(['building: ' filename]);
-        filename = rtwfiles(i);
-        filename = filename{1};
-        rtwbuild(filename);
-    else
-        disp(['skipped:  ' filename]);
-    end
-end
-
-toc;
+build(T, 'all', true);
 
