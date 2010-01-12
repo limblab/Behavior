9e9648a663f0b38b97c4001a7cb29896f8c4e963
Ricardo Torres <ricardort@gmail.com>
2010-01-12 18:38:03 +0000

    Fixed target presentation in bump-choice task.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@529 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

--- src/target/mastercon_bc.c
+++ src/target/mastercon_bc.c
@@ -710,11 +710,27 @@ static void mdlOutputs(SimStruct *S, int_T tid)
         for (i=0; i<4; i++) {
            target_pos[i+1] = ct[i];
         }
-    } else if (state == STATE_CENTER_HOLD || 
-         state == STATE_MOVEMENT  ||
-         state == STATE_BUMP_STIM) {
+    } else if (state ==  STATE_CENTER_HOLD) {
         /* center target on */
         target_pos[0] = 2;
+        for (i=0; i<4; i++) {
+           target_pos[i+1] = ct[i];
+        }
+        /* outer target on */
+        target_pos[5] = 2;
+        for (i=0; i<4; i++) {
+            target_pos[i+6] = rt[i];
+        }
+        if (!training_mode) {
+            target_pos[10] = 2;
+            for (i=0; i<4; i++) {
+                target_pos[i+11] = ft[i];
+            }
+        }
+    } else if ( state == STATE_MOVEMENT  ||
+         state == STATE_BUMP_STIM) {
+        /* center target off */
+        target_pos[0] = 0;
         for (i=0; i<4; i++) {
            target_pos[i+1] = ct[i];
         }
