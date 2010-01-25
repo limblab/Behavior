8b343c42ac8832d95ad1a669c192af476e63e192
Ricardo Torres <ricardort@gmail.com>
2010-01-25 23:51:43 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@536 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

---
+++ src/graphics/Graphics/BumpChoiceStim.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BehaviorGraphics
+{
+    /// <summary>
+    /// Holds a description of the stim table for the bump stim task.
+    /// </summary>
+    public class BumpChoiceStim
+    {
+        private int stimCode;
+        private double PD;
+        
+        /// <summary>
+        /// BumpChoiceStim constructor.  Sets all values to a default of -1.
+        /// </summary>
+        public BumpChoiceStim()
+        {
+            stimCode = -1;
+            PD = -1;
+        }
+
+        public BumpChoiceStim(int sCode, double prefDir)
+        {
+            stimCode = sCode;
+            PD = prefDir;
+        }
+
+        
+        /// <summary>
+        /// Stimulation code
+        /// </summary>
+        [XmlElement("StimCode")]
+        public int StimCodeVar
+        {
+            get { return stimCode; }
+            set { stimCode = value; }
+        }
+
+        /// <summary>
+        /// Preferred direction
+        /// </summary>
+        [XmlElement("PD")]
+        public double PDVar
+        {
+            get { return PD; }
+            set { PD = value; }
+        }
+
+    }
+}
