9e38b517c751cd2636142e26fb64d28ba0d81b11
Matt Bauman <mbauman@gmail.com>
2010-02-24 19:11:29 +0000

    Fix line that was accidentally commented out
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@553 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

--- src/graphics/Graphics/Graphics.Designer.cs
+++ src/graphics/Graphics/Graphics.Designer.cs
@@ -50,7 +50,7 @@ namespace BehaviorGraphics
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            //this.ClientSize = new System.Drawing.Size(541, 360);
+            this.ClientSize = new System.Drawing.Size(541, 360);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Graphics";
