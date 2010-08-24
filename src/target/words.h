0610af322e7c6a8e2360e5b47016074ba5c625d4
Christian Ethier <christian.ethier@gmail.com>
2010-08-24 18:15:18 +0000

    added bump_follow.dlm, modified mastercon_mg to remove aborts during touch_pad_hold state, recompile everything because some dlms had been compiled with a different matlab version and did not load on targets.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@636 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

--- src/target/words.h
+++ src/target/words.h
@@ -24,7 +24,7 @@
 #define BEHAVIOR_VERSION_MAJOR 2
 #define BEHAVIOR_VERSION_MINOR 8
 #define BEHAVIOR_VERSION_MICRO 491
-#define BEHAVIOR_VERSION_BUILD 892
+#define BEHAVIOR_VERSION_BUILD 901
 
 #ifndef __BUILD_UPDATED__
 #error This project must be built from the build script. 
