bdab5a9be610bdfba1215ef3f59c569a505257a4
Brian Dekleva <brian.dekleva@gmail.com>
2011-09-07 16:15:34 +0000

    includes changes to mastercon_un.m and comments in build_all.m
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@718 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

--- src/target/words.h
+++ src/target/words.h
@@ -25,7 +25,7 @@
 #define BEHAVIOR_VERSION_MAJOR 2
 #define BEHAVIOR_VERSION_MINOR 8
 #define BEHAVIOR_VERSION_MICRO 491
-#define BEHAVIOR_VERSION_BUILD 999
+#define BEHAVIOR_VERSION_BUILD 1004
 
 #ifndef __BUILD_UPDATED__
 #error This project must be built from the build script. 
