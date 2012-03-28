8058aef819cd4fadb2bca8838522a8cdcc9017b4
Ricardo Torres <ricardort@gmail.com>
2012-03-28 19:42:34 +0000

    Fixed problem with targets.c linking, Attention behavior runs properly now.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@837 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

--- src/target/words.h
+++ src/target/words.h
@@ -29,7 +29,7 @@
 #define BEHAVIOR_VERSION_MAJOR 2
 #define BEHAVIOR_VERSION_MINOR 8
 #define BEHAVIOR_VERSION_MICRO 491
-#define BEHAVIOR_VERSION_BUILD 1191
+#define BEHAVIOR_VERSION_BUILD 1195
 
 #ifndef __BUILD_UPDATED__
 #error This project must be built from the build script. 
