6a2d06d5c78f0910000bd53140fdcb7a88ad16c1
Ricardo Torres <ricardort@gmail.com>
2012-10-29 15:10:17 +0000

    Minor change to attention behavior.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@1035 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

--- src/target/words.h
+++ src/target/words.h
@@ -84,6 +84,7 @@
 #define __BUILD_UPDATED__ 1
 #define __BUILD_UPDATED__ 1
 #define __BUILD_UPDATED__ 1
+#define __BUILD_UPDATED__ 1
 
 /* 
  * $Id$
@@ -110,7 +111,7 @@
 #define BEHAVIOR_VERSION_MAJOR 2
 #define BEHAVIOR_VERSION_MINOR 8
 #define BEHAVIOR_VERSION_MICRO 491
-#define BEHAVIOR_VERSION_BUILD 1941
+#define BEHAVIOR_VERSION_BUILD 1944
 
 #ifndef __BUILD_UPDATED__
 #error This project must be built from the build script. 
