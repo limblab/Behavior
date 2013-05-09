a1c1144194e1196d2dc513143e5bdf5308d19da0
Ricardo Torres <ricardort@gmail.com>
2013-05-09 17:45:40 +0000

    Changed random_walk motor control to the one in limblab library.  Compiled, not tested.  Added inhibit_motors input to motor control block.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@1106 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

--- src/target/words.h
+++ src/target/words.h
@@ -38,7 +38,7 @@
 #define BEHAVIOR_VERSION_MAJOR 2
 #define BEHAVIOR_VERSION_MINOR 8
 #define BEHAVIOR_VERSION_MICRO 491
-#define BEHAVIOR_VERSION_BUILD 2010
+#define BEHAVIOR_VERSION_BUILD 2011
 
 #ifndef __BUILD_UPDATED__
 #error This project must be built from the build script. 
