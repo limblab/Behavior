bc47e7f0af37cf78b59ffc5480e1786f11a573a2
Ricardo Torres <ricardort@gmail.com>
2015-01-08 21:35:02 +0000

    Added and tested isometric mode for robot tasks to XY position subsytem of limblab library.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/Behavior/trunk@1276 ac8ae79f-f71b-435c-a8fe-0e7694d1e678

--- src/target/words.h
+++ src/target/words.h
@@ -30,6 +30,11 @@
 #define __BUILD_UPDATED__ 1
 #define __BUILD_UPDATED__ 1
 #define __BUILD_UPDATED__ 1
+#define __BUILD_UPDATED__ 1
+#define __BUILD_UPDATED__ 1
+#define __BUILD_UPDATED__ 1
+#define __BUILD_UPDATED__ 1
+#define __BUILD_UPDATED__ 1
 
 /* 
  * $Id$
@@ -56,7 +61,7 @@
 #define BEHAVIOR_VERSION_MAJOR 2
 #define BEHAVIOR_VERSION_MINOR 8
 #define BEHAVIOR_VERSION_MICRO 491
-#define BEHAVIOR_VERSION_BUILD 2712
+#define BEHAVIOR_VERSION_BUILD 2729
 
 #ifndef __BUILD_UPDATED__
 #error This project must be built from the build script. 
