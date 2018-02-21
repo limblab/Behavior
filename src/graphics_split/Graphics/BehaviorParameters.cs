8dc921f2ecb8742ed7e5380a166aba0747344f77
bdek <system administrator@Limblab-Lab3-Graphics.physiology.northwestern.edu>
2018-02-21 16:08:08 -0600

    Adding Anisotropic bump task, split out from the rest of the behaviors

---
+++ src/graphics_split/Graphics/BehaviorParameters.cs
@@ -0,0 +1,342 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BehaviorGraphics
+{
+    [XmlRoot]
+    public class BehaviorParameters
+    {
+        private Version version;
+        private Dictionary<string, double> parameters;
+        private double screenWidth;
+        private int cursor;
+        private int glyphSet;
+        private SoundPlayer.SoundTheme soundTheme;
+        private LoadType lt;
+        private LoadType lt2;
+        private float verticalDisplacement;
+        private int goToneVolume;
+
+        // Multi-Gadget specific elements
+        private List<MultiGadgetTarget> mgTargets;
+        private double[] mgGains = new double[4];
+        private int bdCatchBlock;
+
+        // Flextension specific elements
+        private List<MultiGadgetTarget> wfTargets;
+
+        // Bump choice specific elements
+        private List<BumpChoiceStim> bcStim;
+
+        // Bump follow specific elements
+        private List<BumpChoiceStim> bfStim;
+        
+        // center-out specific elements
+        private CenterOutMode com;
+
+        public List<AnIsoForces> aniso;
+
+        #region publicAttributes
+        
+        public BehaviorParameters()
+        {
+            version = Assembly.GetExecutingAssembly().GetName().Version;
+            parameters = new Dictionary<string, double>();
+            mgTargets = new List<MultiGadgetTarget>();
+            for (int i = 0; i < 16; i++)
+                mgTargets.Add(new MultiGadgetTarget());
+            wfTargets = new List<MultiGadgetTarget>();
+            for (int i = 0; i < 16; i++)
+                wfTargets.Add(new MultiGadgetTarget());
+            bcStim = new List<BumpChoiceStim>();
+            for (int i = 0; i < 16; i++)
+                bcStim.Add(new BumpChoiceStim());
+
+            bfStim = new List<BumpChoiceStim>();
+            for (int i = 0; i < 16; i++)
+                bfStim.Add(new BumpChoiceStim());
+
+            aniso = new List<AnIsoForces>();
+            for (int i = 0; i < 16; i++)
+            {
+                aniso.Add(new AnIsoForces());
+            }
+
+        }
+
+        [XmlIgnore]
+        public Dictionary<string, double> Parameters
+        {
+            set { parameters = value; }
+            get { return parameters; }
+        }
+
+        [XmlElement]
+        public double ScreenWidth
+        {
+            set { this.screenWidth = value; }
+            get { return this.screenWidth; }
+        }
+
+        [XmlElement]
+        public int GoToneVolume
+        {
+            get { return this.goToneVolume; }
+            set { this.goToneVolume = value; }
+        }
+
+        [XmlArray]
+        public double[] MGGains
+        {
+            set {this.mgGains = value; }
+            get { return this.mgGains; }
+
+        }
+
+        [XmlElement]
+        public int BD_Catch_Block
+        {
+            set { this.bdCatchBlock = value; }
+            get { return this.bdCatchBlock; }
+        }
+
+        [XmlElement]
+        public float VerticalDisplacement
+        {
+            get { return this.verticalDisplacement; }
+            set { this.verticalDisplacement = value; }
+        }
+
+        [XmlElement]
+        public int Cursor
+        {
+            set { this.cursor = value; }
+            get { return this.cursor; }
+        }
+
+        [XmlElement]
+        public int GlyphSet {
+            set { this.glyphSet = value; }
+            get { return this.glyphSet; }
+        }
+
+        [XmlElement]
+        public SoundPlayer.SoundTheme SoundTheme
+        {
+            set { this.soundTheme = value; }
+            get { return this.soundTheme; }
+        }
+
+        [XmlElement]
+        public CenterOutMode CenterOutMode
+        {
+            set { com = value; }
+            get { return com; }
+        }
+
+        [XmlElement]
+        public LoadType Load
+        {
+            set { lt = value; }
+            get { return lt; }
+        }
+
+        [XmlElement]
+        public LoadType LoadCatch
+        {
+            set { lt2 = value; }
+            get { return lt2; }
+        }
+ 
+
+
+        [XmlAttribute]
+        public string Version
+        {
+            get { return this.version.ToString(); }
+            set { this.version = new Version(value); }
+        }
+
+        [XmlElement]
+        public DictionarySerializer SerializedParameters
+        {
+            get { return new DictionarySerializer(parameters); }
+            set { parameters = (Dictionary<string, double>)value.Dictionary; }
+        }
+
+        [XmlArray("MultiGadgetTargetList")]
+        public MultiGadgetTarget[] MGTArray
+        {
+            get
+            {
+                MultiGadgetTarget[] mgta = new MultiGadgetTarget[mgTargets.Count];
+                mgTargets.CopyTo(mgta);
+                return mgta;
+            }
+            set
+            {
+                if (value == null) return;
+                MultiGadgetTarget[] mgta = (MultiGadgetTarget[])value;
+                mgTargets.Clear();
+                foreach (MultiGadgetTarget mgt in mgta)
+                {
+                    mgTargets.Add(mgt);
+                }
+            }
+        }
+
+        [XmlIgnore]
+        public List<MultiGadgetTarget> MGTargets
+        {
+            get { return mgTargets; }
+            set { mgTargets = value; }
+        }
+
+        [XmlIgnore]
+        public List<AnIsoForces> AnIsotropicForces
+        {
+            get { return aniso; }
+            set { aniso = value; }
+        }
+        [XmlArray("FlextensionTargetList")]
+        public MultiGadgetTarget[] WFTArray
+        {
+            get
+            {
+                MultiGadgetTarget[] wfta = new MultiGadgetTarget[wfTargets.Count];
+                wfTargets.CopyTo(wfta);
+                return wfta;
+            }
+            set
+            {
+                if (value == null) return;
+                MultiGadgetTarget[] wfta = (MultiGadgetTarget[])value;
+                wfTargets.Clear();
+                foreach (MultiGadgetTarget wft in wfta)
+                {
+                    wfTargets.Add(wft);
+                }
+            }
+        }
+        
+        [XmlIgnore]
+        public List<MultiGadgetTarget> WFTargets
+        {
+            get { return wfTargets; }
+            set { wfTargets = value; }
+        }
+
+        [XmlArray("BumpChoiceStimList")]
+        public BumpChoiceStim[] BCSList
+        {
+            get
+            {
+                BumpChoiceStim[] bcsl = new BumpChoiceStim[bcStim.Count];
+                bcStim.CopyTo(bcsl);
+                return bcsl;
+            }
+            set
+            {
+                if (value == null) return;
+                BumpChoiceStim[] bcsl = (BumpChoiceStim[])value;
+                bcStim.Clear();
+                foreach (BumpChoiceStim bcs in bcsl)
+                {
+                    bcStim.Add(bcs);
+                }
+            }
+        }
+
+        [XmlIgnore]
+        public List<BumpChoiceStim> BCStims
+        {
+            get { return bcStim; }
+            set { bcStim = value; }
+        }
+
+        [XmlArray("BumpFollowStimList")]
+        public BumpChoiceStim[] BFSList
+        {
+            get
+            {
+                BumpChoiceStim[] bfsl = new BumpChoiceStim[bfStim.Count];
+                bfStim.CopyTo(bfsl);
+                return bfsl;
+            }
+            set
+            {
+                if (value == null) return;
+                BumpChoiceStim[] bfsl = (BumpChoiceStim[])value;
+                bfStim.Clear();
+                foreach (BumpChoiceStim bfs in bfsl) {
+                    bfStim.Add(bfs);
+                }
+            }
+        }
+
+        [XmlIgnore]
+        public List<BumpChoiceStim> BFStims
+        {
+            get { return bfStim; }
+            set { bfStim = value; }
+        }
+
+        #endregion
+        }
+    
+
+        public enum CenterOutMode
+        {
+            Bump = 2,
+            Catch = 1,
+            None = 0
+        }
+
+        public enum LoadType
+        {
+            None = 0,
+            Static = 1,
+            Viscous = 2,
+            Curl = 3,
+            RotatingCurl = 4,
+            Chaotic = 5,
+            Elastic = 6
+        }
+
+        public enum LoadParam
+        {
+            None = 0,
+            Static = 1,
+            Chaotic = 2,
+            Viscous = 3,
+            Elastic = 4
+        }
+
+        public enum BD_Catch_List
+        {
+            None  = 0,
+            pct5  = 20,
+            pct10 = 10,
+            pct14 = 7,
+            pct20 = 5,
+            pct25 = 4,
+            pct33 = 3,
+        }
+        public enum BD_Catch_Index
+        {
+            None = 0,
+            pct5 = 1,
+            pct10 = 2,
+            pct14 = 3,
+            pct20 = 4,
+            pct25 = 5,
+            pct33 = 6,
+        }
+
+}
+
---
+++ src/graphics_split/Graphics/BoxConfig.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BehaviorGraphics
+{
+    public partial class BoxConfig : Form
+    {
+        private Box box;
+
+        public BoxConfig()
+        {
+            InitializeComponent();
+        }
+
+        #region Form Event Callbacks
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+            this.Dispose();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+            this.Dispose();
+        }
+
+        private void textBox_Leave(object sender, EventArgs e)
+        {
+            float position;
+            try {
+                TextBox tb = (TextBox)sender;
+                position = (float)Double.Parse(tb.Text);
+
+                if (tb.Text.Equals(textBoxTop.Text)) {
+                    box.Top = position;
+                }
+                if (tb.Text.Equals(textBoxBottom.Text)) {
+                    box.Bottom = position;
+                }
+                if (tb.Text.Equals(textBoxLeft.Text)) {
+                    box.Left = position;
+                }
+                if (tb.Text.Equals(textBoxRight.Text)) {
+                    box.Right = position;
+                }
+            } catch {
+                MessageBox.Show("Value must be a position in cm", "Format Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+        #endregion
+
+        #region Public Attributes
+        public Box Box
+        {
+            get
+            {
+                return box;
+            }
+            set
+            {
+                box = value;
+                textBoxBottom.Text = String.Format("{0}", box.Bottom);
+                textBoxTop.Text = String.Format("{0}", box.Top);
+                textBoxLeft.Text = String.Format("{0}", box.Left);
+                textBoxRight.Text = String.Format("{0}", box.Right);
+            }
+        }
+        #endregion
+    }
+}
