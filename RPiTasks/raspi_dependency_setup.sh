1317df3f53514097ebaa005e49062249a83be1eb
xuanma <maxuan.1987@163.com>
2020-03-17 15:38:38 -0500

    bash script to take care of getting all of the necessary libraries running on the raspberry Pi

---
+++ RPiTasks/raspi_dependency_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install libatlas-base-dev
+sudo apt-get install vim
+
+sudo pip3 install numpy pandas gpiozero pygame pynput spidev
+sudo apt-get install cowsay
