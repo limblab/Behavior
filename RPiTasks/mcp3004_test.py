55c7c5250ae55062c4bd19598179a10acaad8f82
RPi Cage <miller.limblab@gmail.com>
2020-05-05 17:09:04 -0500

    updates to the mcp test code -- made when checking it on the pi

--- RPiTasks/mcp3004_test.py
+++ RPiTasks/mcp3004_test.py
@@ -12,13 +12,21 @@ Created on Wed Feb 26 14:14:44 2020
 """
 
 import spidev, gpiozero, time
-import matplotlib as plt
+from matplotlib import pyplot as plt
 import pandas as pd
 
-mcp0 = gpiozero.mcp3004(channel=0,differential=False,max_voltage=5,device=0)
-mcp1 = gpiozero.mcp3004(channel=1,differential=False,max_voltage=5,device=0)
-mcp2 = gpiozero.mcp3004(channel=2,differential=False,max_voltage=5,device=0)
-mcp3 = gpiozero.mcp3004(channel=3,differential=False,max_voltage=5,device=0)
+
+plt.ion()
+
+
+mcp0 = gpiozero.MCP3004(channel=0,differential=False,max_voltage=5,device=0)
+mcp1 = gpiozero.MCP3004(channel=1,differential=False,max_voltage=5,device=0)
+mcp2 = gpiozero.MCP3004(channel=2,differential=False,max_voltage=5,device=0)
+mcp3 = gpiozero.MCP3004(channel=3,differential=False,max_voltage=5,device=0)
+
+
+time.sleep(.5)
+
 
 fig,ax = plt.subplots()
 mcp0Val = list()
@@ -28,24 +36,51 @@ mcp3Val = list()
 
 ii = 0
 
+
+ln0, = ax.plot([], [], label='mcp0')
+ln1, = ax.plot([], [], label='mcp1')
+ln2, = ax.plot([], [], label='mcp2')
+ln3, = ax.plot([], [], label='mcp3')
+ax.legend()
+ax.set_ylim(-0,1, 1.1)
+ax.set_xlim(-5, 105)
+
+
+
+
+
 while True:
-    ii = ii + 1
     
-    mcp0Val = mcp0Val.append(mcp0.value())
-    mcp1Val = mcp1Val.append(mcp1.value())
-    mcp2Val = mcp2Val.append(mcp2.value())
-    mcp3Val = mcp3Val.append(mcp3.value())
+    mcp0Val.append(mcp0.value)
+    mcp1Val.append(mcp1.value)
+    mcp2Val.append(mcp2.value)
+    mcp3Val.append(mcp3.value)
 
     if ii > 100:
+        ii = 100
         mcp0Val.pop(0)
         mcp1Val.pop(0)
         mcp2Val.pop(0)
         mcp3Val.pop(0)
         
-        
-    ax.plot([mcp0Val,mcp1Val,mcp2Val,mcp3Val],list(range(ii)),label=['mcp0','mcp1','mcp2','mcp3'])
-    ax.legend()
     
+    print(mcp0Val[ii],mcp1Val[ii],mcp2Val[ii],mcp3Val[ii],sep='\t',end='\n')
+
     
+
+    # ax.plot(list(range(ii+1)),mcp0Val,label='mcp0')
+    # ax.legend()
+    ln0.set_data(list(range(ii+1)),mcp0Val)
+    ln1.set_data(list(range(ii+1)),mcp1Val)
+    ln2.set_data(list(range(ii+1)),mcp2Val)
+    ln3.set_data(list(range(ii+1)),mcp3Val)
+
+
+
+
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
+    ii = ii + 1
     
     time.sleep(.1)
