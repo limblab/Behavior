86e0847747c0082e4d8dd18864d3d09db24fb3a0
kb0dkin <kb0dkin@github.com>
2020-05-05 15:27:27 -0500

    updating the MCP3004 test to output a plot instead of random numbers

--- RPiTasks/mcp3004_test.py
+++ RPiTasks/mcp3004_test.py
@@ -12,12 +12,40 @@ Created on Wed Feb 26 14:14:44 2020
 """
 
 import spidev, gpiozero, time
+import matplotlib as plt
+import pandas as pd
 
 mcp0 = gpiozero.mcp3004(channel=0,differential=False,max_voltage=5,device=0)
 mcp1 = gpiozero.mcp3004(channel=1,differential=False,max_voltage=5,device=0)
 mcp2 = gpiozero.mcp3004(channel=2,differential=False,max_voltage=5,device=0)
 mcp3 = gpiozero.mcp3004(channel=3,differential=False,max_voltage=5,device=0)
 
+fig,ax = plt.subplots()
+mcp0Val = list()
+mcp1Val = list()
+mcp2Val = list()
+mcp3Val = list()
+
+ii = 0
+
 while True:
-    print([mcp0.value(), mcp1.value(), mcp2.value(), mcp3.value()])
+    ii = ii + 1
+    
+    mcp0Val = mcp0Val.append(mcp0.value())
+    mcp1Val = mcp1Val.append(mcp1.value())
+    mcp2Val = mcp2Val.append(mcp2.value())
+    mcp3Val = mcp3Val.append(mcp3.value())
+
+    if ii > 100:
+        mcp0Val.pop(0)
+        mcp1Val.pop(0)
+        mcp2Val.pop(0)
+        mcp3Val.pop(0)
+        
+        
+    ax.plot([mcp0Val,mcp1Val,mcp2Val,mcp3Val],list(range(ii)),label=['mcp0','mcp1','mcp2','mcp3'])
+    ax.legend()
+    
+    
+    
     time.sleep(.1)
