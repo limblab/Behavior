45976a7644eb08b299958d2a49702822fad115e3
RPi Cage <miller.limblab@gmail.com>
2020-06-25 17:53:15 -0500

    Changes to the button task and addition of a proximity sensor task

--- RPiTasks/ButtonTask.py
+++ RPiTasks/ButtonTask.py
@@ -17,22 +17,61 @@ from pandas import DataFrame as df
 import gpiozero as gp
 
 
-
+# ---------------------------------------------------------------------------------------------#
 # initialize all of the spi stuff
+
+# task buttons
 buttonsDict = {
-        'signal': [gp.Button(2,pull_up=False,hold_time=.5),gp.Button(14,pull_up=False,hold_time=.5),
-        'LED': [gp.LED(3),gp,LED(10)]}
+        'signal': [gp.Button(2,hold_time=.5),gp.Button(14,pull_up=False,hold_time=.5)],
+        'LED': [gp.LED(3),gp.LED(15)]}
 buttons = df(buttonsDict)
 
+# reward buttons
+rButtonDict = {
+        'signal': [gp.Button(18,pull_up=False,hold_time=.1),gp.Button(10,pull_up=False,hold_time=.1)],
+        'LED': [gp.LED(17),gp.LED(22)],
+        'sol': [gp.DigitalOutputDevice(20),gp.DigitalOutputDevice(21)]}
+rButtons = df(rButtonDict)
 
 
+
+# ---------------------------------------------------------------------------------------------#
 ''' 
 defining all of the subfunctions 
 
 there are going to be functions for each of the states, and a function to 
 figure out wait times etc when we start a new trial.
 '''
-# setup the different times
+# button press function
+def bPressFn(button,buttons,rButtons):
+    buttons['LED'][:].off() 
+    buttons['signal'][:].when_held = None
+    rButtons['LED'][:].on()
+    rButtons['signal'][:].when_held = rewardFn()
+
+
+def rewardFn(button,rButtons,dispTime):
+    # turn off the leds and the reward buttons
+    rButtons['LED'][:].off()
+    rButtons['signal'][:].when_held = None
+    
+    # dispense the appropriate solenoid
+    if button.pin == 'GPIO18':
+        rButtons['sol'][0].on()
+    elif button.pin == 'GPIO10':
+        rButtons['sol'][1].on()
+    
+    # dispense for X time, then turn the rewards off
+    time.sleep(dispTime)
+    rButtons['sol'][:].off()
+
+
+
+
+
+
+
+# setup required times for each trial
 def setupTrial(touchTimeM,dispenseTimeM,interTrialM,numButtons):
     ''' 
     touchTime -- amount of time to touch the button
@@ -44,38 +83,15 @@ def setupTrial(touchTimeM,dispenseTimeM,interTrialM,numButtons):
     touchTime = random.uniform(touchTimeM[0],touchTimeM[1])
     dispenseTime = random.uniform(dispenseTimeM[0],dispenseTimeM[1])
     interTrial = random.uniform(interTrialM[0],interTrialM[1])
-    buttonNumber = random.choices(range(0,4),k=numButtons)
+    buttonNumber = random.choices(range(0,2),k=numButtons)
     
     return [touchTime, dispenseTime, interTrial, buttonNumber]
 
 
 
-# checking to see whether the button has been pressed long enough
-def ready(currButton, touchTime):
-    
-    pressedButton = 1
-    # check to see whether the buttons have been pressed for the length of time they need
-    for b in currButton:
-        if button_touched(b): # first make sure it's not None (it will throw an error with a >)
-            if b.held_time > touchTime: # if it has been held long enough
-                pressedButton = pressedButton & 1
-            else:
-                pressedButton = pressedButton & 0
-            
-    return pressedButton
 
 
-# ask the device if the button is being touched
-def button_touched(button_num):
-    
-    
-    
-    
-    return 1
-    
-
-
-
+# ---------------------------------------------------------------------------------------------#
 # initalize the mixer to create the sounds
 # why is the sampling frequency so obnoxious? dunno...
 pygame.mixer.init(frequency=8192,buffer=4096)
@@ -102,6 +118,8 @@ current status variables:
     interTrialM - inter trial min and max
     
 '''
+
+'''
 STATE_READY = 0
 STATE_TOUCH = 1
 STATE_READY_DISPENSE = 2
@@ -115,14 +133,47 @@ touchTimeM = [0.25, 0.5] # range of time they have to hold the buttons
 dispenseTimeM = [0.1, 0.2] # length of dispense time -- going to be short since they'll be in the cage all day
 interTrialM = [1.0, 15.0] # time (in seconds) between trials
 
-[touchTime, dispenseTime, interTrial, buttonNumber] = setupTrial(touchTimeM, dispenseTimeM, interTrialM, numButtons)
-ledTuple(buttonNumber).on()
+'''
 
 
+random.seed
+numButtons = 1 # the number of buttons that we want simultaneously pressed. gonna be max 2
+touchTimeM = [0.25, 0.5] # range of time they have to hold the buttons
+dispenseTimeM = [0.1, 0.2] # length of dispense time -- going to be short since they'll be in the cage all day
+interTrialM = [1.0, 15.0] # time (in seconds) between trials
+
+
+
+# ---------------------------------------------------------------------------------------------#
 # start the infinite loop
 while True:
-    pygame.event.get() # clears the pygame queue to prevent queue buildup
+    # pygame.event.get() # clears the pygame queue to prevent queue buildup
+     
+    [touchTime, dispenseTime, interTrial, buttonNumber] = setupTrial(touchTimeM, dispenseTimeM, interTrialM, numButtons)
     
+
+    # set up the press function for the buttons, then wait until they're pressed
+    buttons['LED'][numButtons].on() # turn on some buttonz
+    goSound.play() # beep
+    buttons['signal'][numButtons].hold_time = touchTime
+    buttons['signal'][numButtons].when_pressed = bPressFn(button,buttons,rButtons)
+    buttons['signal'][numButtons].wait_for_active()
+    
+    # set up the reward function for the buttons, then wait until they aren't pressed 
+    goSound.play() # beep
+    rButtons['LED'][:].on()
+    rButtons['signal'][:].when_pressed = rewardFn(button,rButtons,dispenseTime)
+    rButtons['signal'][:].wait_for_active()
+    rewardSound.play()
+
+    # test it out
+    time.sleep(interTrial)
+
+    
+    
+    
+    
+    ''' 
     if state == STATE_READY:
         if ready(button, touchTime): 
             goSound.play() # beep
@@ -142,7 +193,7 @@ while True:
         time.sleep(interTrial)
         [touchTime, dispenseTime, interTrial, buttonNumber] = setupTrial(touchTimeM, dispenseTimeM, interTrialM, numButtons)
 
-            
+    '''        
         
 
 
---
+++ RPiTasks/ProxTask.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+ButtonTask()
+
+Multi-button task for the cage. Basic layout is a square with a bunch of 
+different buttons. Press a button (or two) when it lights up. After a certain
+number of successful button presses, turn on the reward panel.
+
+This is implemented for Raspberry Pi. Go ahead and try to get it to work on 
+other systems -- using the GPIO libraries will be kind of funny.
+"""
+
+# import all needed modules
+import pygame, random, os
+from time import sleep
+from pandas import DataFrame as df
+import gpiozero as gp
+
+pygame.init()
+
+# ---------------------------------------------------------------------------------------------#
+# initialize manipulanda stuff
+
+# task buttons
+proxDict = {
+        'signal': gp.LineSensor(14,pull_up=False),
+        'irLED': gp.LED(15)}
+prox = df(proxDict,index=[0])
+prox['irLED'][0].on()
+
+
+# reward buttons
+rButtonDict = {
+        'signal': [gp.Button(18,pull_up=False,hold_time=.1)],
+        'LED': [gp.LED(17)],
+        'sol': [gp.DigitalOutputDevice(20)]}
+rButton = df(rButtonDict)
+
+
+
+
+# ---------------------------------------------------------------------------------------------#
+# initalize the mixer to create the sounds
+# why is the sampling frequency so obnoxious? dunno...
+pygame.mixer.init(frequency=163840,buffer=32000)
+#pygame.mixer.init(frequency=16840,buffer=32000)
+goSound = pygame.mixer.Sound(os.path.join("tones","go3_interp.wav"))
+rewardSound = pygame.mixer.Sound(os.path.join("tones","reward3.wav"))
+
+'''
+
+# ---------------------------------------------------------------------------------------------#
+# intialize the screen so we can have colors etc
+size = [800, 400]
+screen = pygame.display.set_mode(size)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+YELLOW = (255,255,0)
+BLUE = (0,0,255)
+GREEN = (0,255,0) 
+
+
+screen.fill(BLACK)
+
+'''
+# ---------------------------------------------------------------------------------------------#
+# start the infinite loop
+while True:
+    # pygame.event.get() # clears the pygame queue to prevent queue buildup
+   
+    # screen.fill(BLUE) 
+    # pygame.display.update() 
+   
+   
+    prox['signal'][0].wait_for_active()
+    print('Monkey in the corner!')
+    # screen.fill(YELLOW)
+    # pygame.display.update()
+    rewardSound.play()
+
+    rButton['LED'][0].on()
+    rButton['signal'][0].wait_for_press()
+    rButton['sol'][0].on()
+    sleep(.1)
+    rButton['sol'][0].off()
+
+    rButton['LED'][0].off()
+
+
+
+
+
+    # screen.fill(GREEN)
+    # pygame.display.update()
+
+    print('reward given')
+
