c0715b1d0d05e66a20e527b86efd06284cdc869d
kb0dkin <kb0dkin@github.com>
2020-07-22 23:27:16 -0500

    finished all of the initialization and changes to class based definitions of targets and cursors. Started on the state machine

--- RPiTasks/MultiGadget.py
+++ RPiTasks/MultiGadget.py
@@ -25,59 +25,23 @@ Notes on storage of task performance:
 """
 
 # import all needed modules
-import time, sys, os, pygame, random, spidev, gpiozero, pandas, csv
+import sys, os, pygame, random, gpiozero, csv
 from datetime import datetime as dt
-from math import sin, pi, cos
-# from pynput.mouse import Controller
+from time import sleep
 
 
-# set up gpiozero pins for the task and reward buttons -- gave up on the linear shift register
-taskButtons = [gpiozero.Button(2), gpiozero.Button(14)]  # Button0: GPIO2; Button1: GPIO14
-taskLEDs = [gpiozero.LED(3), gpiozero.LED(15)] # Button0LED: GPIO3; Button1LED: GPIO15
-rewardButtons = [gpiozero.Button(18), gpiozero.Button(23)] # Reward0Button: GPIO18; Reward1Button: GPIO23
-rewardLEDs = [gpiozero.LED(17), gpiozero.LED(22)] # Reward0LED: GPIO17; Reward1LED: GPIO22
+""" ##########################################################################
+### Define 'constants'
+###########################################################################"""
+# screen and color values
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+SIZE = [800, 400]
 
-
-
-''' Prep everything for the ADC -- MCP3004
-Just using the MCP3004 predefined device from gpiozero. Thanks gpiozero!
-'''
-devices = pandas.DataFrame(columns=['DeviceZero','DeviceOne'],index=['FSROne','FSRTwo','LED'])
-devices.DeviceZero.FSROne = gpiozero.MCP3004(channel=0,device=0)
-devices.DeviceZero.FSRTwo = gpiozero.MCP3004(channel=1,device=0)
-devices.DeviceZero.LED = gpiozero.LED(16)
-devices.DeviceOne.FSROne = gpiozero.MCP3004(channel=2,device=0)
-devices.DeviceOne.FSRTwo = gpiozero.MCP3004(channel=3,device=0)
-devices.DeviceOne.LED = gpiozero.LED(12)
-
-
-#cursors = pandas.DataFrame(columns=['CursorOne','CursorTwo'],index=['X','Y'])
-
-
-'''
-State variables
-
-states:
-    STATE_START_TRIAL
-    STATE_MOVEMENT
-    STATE_REWARD
-    STATE_BETWEEN_TRIALS
-
-'''
-
-STATE_START_TRIAL = 0
-STATE_MOVEMENT = 1
-STATE_REWARD = 2
-STATE_BETWEEN_TRIALS = 3
-
-state = STATE_BETWEEN_TRIALS
-
-''' 
-Word codes
-
-These are the same words that we have defined in all of the xpc stuff. This way
-we can just use all of the previous code on the analysis side.
-'''
+# word codes -- the same as our XPC defined word codes, just to make life easy
 WORD_START_TRIAL = 0x16 # generic trial start: 0x10; MG: 0x06
 WORD_END_TRIAL = 0x20 
 WORD_REWARD = 0x00 | WORD_END_TRIAL
@@ -89,61 +53,24 @@ WORD_GO_CUE = 0x31
 WORD_CATCH = 0x32
 
 
+# state machine
+STATE_START_TRIAL = 0
+STATE_MOVEMENT = 1
+STATE_REWARD = 2
+STATE_BETWEEN_TRIALS = 3
 
-''' 
-storage of the words and cursor locations
-'''
-wordFile = 'home/pi/Documents/taskOutput/' + dt.now().strftime('%Y%m%d_%H:%M:%S') + '_cage_word_file.csv'
-wordWriter = csv.writer(open(wordFile))
-
-
-''' 
---- important variables ---
-(could consider loading some of these from and external settings file)
-    targets: location of all of the targets. Dataframe with location of centers and sizes of boxes 
-    targetHoldTime: range of target hold times (in seconds). Set with two values
-    dispenseTime: range of dispense hold times (in seconds). Set with two values
-    interTrial: range of length between trials (in seconds). Set with two values
-
-    target: location of current target; it's a class with associated color etc defined below
-    cTargetHoldTime: current length of time to hold the target (in seconds)
-    cDispenseTime: current range of dispense hold times (in seconds)
-    cInterTrial: current range of length between trials (in seconds)
-
-    gain: multiplier for the cursor -- tuple for the two cursors
-    offset: offset for the cursor -- tuple for the two cursors
-
-    cursorLocn: gain*force + offset  -- should we consider adding some filtering? maybe...
-'''
-
-
-targets = pandas.DataFrame(columns=['one','two','three'],index=['x','y','width','height'])
-targetHoldTime = [.01 * ii for ii in range(25,75)]
-dispenseTime = [.01 * ii for ii in range(5,15)]
-interTrial = [.1 * ii for ii in range(15, 50)]
-
-gain = (10)
-offset = (-5)
-
-graspType = [] # this will need to be filled out by the user -- figure out some good way to have a settings file
-
-
-#initialize some screen stuff
-size = [800,480] # size of the 7" pi screen
-screen = pygame.display.set_mode(size)
-BLACK = (0,0,0)
-WHITE = (255, 255, 255)
-YELLOW = (255, 255, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-cursImage = [pygame.image.load('./textures/face.tga'), pygame.image.load('./textures/face.tga')]
-cursRect = [jj.get_rect() for jj in cursImage]
+state = STATE_BETWEEN_TRIALS
 
 
 
 
-#define target 
+""" ##########################################################################
+### Define all classes and submodules
+###########################################################################"""
+
+### define target 
 class target():
+        
     def __init__(self,x,y,width,height):
         self.color = RED
         self.x = x
@@ -154,6 +81,8 @@ class target():
     def draw(self, screen):
         # pygame draws rectangular sprites from the top left corner, but we 
         # want to keep track of things by the center hence the offsets
+        X = (800/6)*cursX + 400 # screen is 6 in wide, 800 pixels -- switch from inches to pixels
+        Y = (-480/3.25)*cursY + 240 # screen is 3.25 in tall, 480 pixels -- inches to pixels and flip direction
         pygame.draw.rect(screen, self.color, (self.x + self.width/2, self.y + self.height/2, self.width, self.height))
         
     def isOver(self, cursRect):
@@ -167,63 +96,151 @@ class target():
         #         return True                            
         return False
 
+### Define devices -- this is only for ADC devices that use FSRs for now
+class device():
+    # initialize
+    def __init__(self,name,FSR0,FSR1,gain,offset,LED,imagePath):
+        self.name = name # device name. Usually just power grasp etc
+        self.FSR = [FSR0, FSR1] # these need to be two instances of MCP devices -- from the gpiozero library
+        self.gain = gain # plan on this being an int for now
+        self.offset = offset # plan on this being an int for now -- vertical gain from center. This should be negative
+        self.LED = [LED] # allow for multiple LEDs
+        self.cursImage = pygame.image.load(imagePath) # the image for our cursor
+        self.cursRect = self.cursImage.get_rect() # the pygame rect object for our cursor
+        self.cursRect.center = [0,offset]
+
+    # update the cursor location and display
+    def update_cursor(self,screen):
+        cursX = (self.FSR[1].value - self.FSR[0].value) * self.gain
+        cursY = (self.FSR[0].value + self.FSR[1].value) * self.gain + offset
+        X = (800/6)*cursX + 400 # screen is 6 in wide, 800 pixels -- switch from inches to pixels
+        Y = (-480/3.25)*cursY + 240 # screen is 3.25 in tall, 480 pixels -- inches to pixels and flip direction
+        self.cursRect.center = [X,Y]
+        screen.fill(BLACK)
+        screen.blit(self.cursImage,self.cursRect)
+        pygame.display.flip()
+        
+    def activate_device(self):
+        for led in self.LED:
+            led.on() # turn the LED on
+        
+    def deactivate_device(self):
+        for led in self.LED:
+            led.off()
 
 
-# defining modules
 
-def redrawWindow(): # redraws the window as black
+### defining modules
+# redraw the window
+def blank_screen(screen): # redraws the window as black
     screen.fill(BLACK)
-    mainTarget.draw(screen)
+    pygame.display.flip()
 
 
+### choose new device and target
+def restart_task(devDict,tgtDict):
+    # choose the device
+    dev = devDict[random.choice(list(devices.keys()))]
+    tgt = tgtDict[random.choice(list(targets.keys()))]
 
-def get_cursor_locn(devices,gain=[1,1],offset=[-1,-1]):
-    cursors = pandas.DataFrame(columns=['CursorOne','CursorTwo'],index=['X','Y'])
+    return dev,tgt
+
+### random delay generators -- so that it's a little cleaner in the main loop
+class delayGenerator():
+    def __init__(self,min,max):
+        self.min = min
+        self.max = max
+        self.current = (random.random() * (max-min)) + min
+        
+    def reroll(self):
+        self.current = (random.random() * (self.max-self.min)) + self.min
+
+
+"""##############################################################################
+### Device and button list initialization
+##############################################################################"""
+# set up gpiozero pins for the task and reward buttons -- gave up on the linear shift register
+# button 0: button is gpio2, LED is gpio3. button 0 doesn't allow a PD resistor! whoops!
+# button1: button is gpio14, LED is gpio15
+# rewardButton 0: button is gpio18, LED is gpio17
+# rewardButton 1: button is gpio23, LED is gpio22
+taskButtons = {'ProximitySensor': 
+               {'Button': gpiozero.Button(14),
+                'LED': gpiozero.LED(15)}}
+
+rewardButtons = {'Water':
+                 {'Button': gpiozero.Button(18),
+                  'LED': gpiozero.LED(17)}}
     
-    
-    # take in the device information along with gains, and return the cursor locations (in inch coordinates)
-    cursors.CursorOne.X = (devices.DeviceZero.FSROne.value * .5 - devices.DeviceZero.FSRTwo.value * .5) * gain[0] - 1.5
-    cursors.CursorOne.Y = (devices.DeviceZero.FSROne.value * .5 + devices.DeviceZero.FSRTwo.value * .5) * gain[0] + offset[0]
-    cursors.CursorTwo.X = (devices.DeviceOne.FSROne.value * .5 - devices.DeviceOne.FSRTwo.value * .5) * gain[0] + 1.5
-    cursors.CursorTwo.Y = (devices.DeviceOne.FSROne.value * .5 + devices.DeviceOne.FSRTwo.value * .5) * gain[0] + offset[0]
-    
-    return cursors
+
+### Initialize the devices, put them in a dictionary together
+# Just using the MCP3004 predefined device from gpiozero. Thanks gpiozero!
+# devices.DeviceZero.FSROne = gpiozero.MCP3004(channel=0,device=0)
+# devices.DeviceZero.FSRTwo = gpiozero.MCP3004(channel=1,device=0)
+# devices.DeviceZero.LED = gpiozero.LED(16)
+# devices.DeviceOne.FSROne = gpiozero.MCP3004(channel=2,device=0)
+# devices.DeviceOne.FSRTwo = gpiozero.MCP3004(channel=3,device=0)
+# devices.DeviceOne.LED = gpiozero.LED(12)
+
+devDict = {'Powergrasp': device('Powergrasp',gpiozero.mcp3004(channel=0,device=0),
+                                gpiozero.mcp3004(channel=0,device=1),
+                                10,-2,gpiozero.LED(16),'./textures/face.tga')}
+
+
+### initialize the targets with desired locations (in inches)
+tgtDict = {1: target(0,1,4,.5),
+           2: target(0,2,4,.5),
+           3: target(0,2.5,4,.5)}
+
+
+### initialize all of the wait times
+targetHoldTime = delayGenerator(.25, .75) # how long do they have to be in the target?
+dispenseTime = delayGenerator(.5, 1) # time to receive the reward
+interTrialTime = delayGenerator(1.5, 5) # time between trials
 
 
 
-def cursor_to_screen(X,Y): # converts the cursor XY coordinates to screen XY coordinates. 
-    # screen is approx 6W x 3.25H, 480H x 800W pixels
-    Y = (-480/3.25)*Y + 240 # flip the direction of incrementing for Y
-    X = (800/6)*X + 400
-    return [X, Y] # return the converted versions
+
+'''###########################################################################
+### Initialize pygame -- display, sounds etc
+###########################################################################'''
+
+### initialize some screen stuff
+pygame.init()
+screen = pygame.display.set_mode(size=SIZE,flags=(pygame.FULLSCREEN|pygame.NOFRAME))
+blank_screen(screen)
+pygame.event.clear()
+
+### initialize the sound stuff
+pygame.mixer.init(frequency=163840,buffer=32000) # mister owl, why are these sampling frequencies so weird?
+goSound = pygame.mixer.Sound(os.path.join("tones","go3_interp.wav"))
+rewardSound = pygame.mixer.Sound(os.path.join("tones","reward3_interp.wav"))
 
 
 
 
-
+''' 
+storage of the words and cursor locations
+'''
+wordFile = 'home/pi/Documents/taskOutput/' + dt.now().strftime('%Y%m%d_%H:%M:%S') + '_cage_word_file.csv'
+wordWriter = csv.writer(open(wordFile))
 
 
 
 # let's get ready to loop
-cont = True
-clock = pygame.time.Clock()
-# mouse = Controller()
 
 
-tInit = time.perf_counter_ns()
-tCurrState = time.perf_couter_ns()
+
 while cont:
-    pygame.event.get() # clears the pygame queue to prevent queue buildup
+    for ev in pygame.event.get():
+        if event.type == pygame.quit:
+            sys.exit()
+    
  
     
     if state = STATE_START_TRIAL: # if we're starting a new trial, mark it and let us know
-        iTarget = random.randint(0,len(targets.columns)-1)
-        tgt = target(targets.iloc[0,iTarget],targets.iloc[1,iTarget],targets.iloc[2,iTarget],targets.iloc[3,iTarget])
-        iDevice = random.randint(0,len(devices.columns)-1)
-        cDevice = 
-        wordWriter.writerow([time.perf_counter_ns-tInit,WORD_START_TRIAL]
-        state = STATE_MOVEMENT
-        tCurrState = time.perf_couter_ns()
+        
+        
         
     elif state = STATE_MOVEMENT
             
@@ -234,51 +251,17 @@ while cont:
         for ii in cursRect:
             ii.
      
-     
-     
-     
-     
- #   clock.tick(60)
-    redrawWindow()
- #   fakeCursorButton(button)
-    pygame.display.flip
-    
-    
-    # for event in pygame.event.get():
-    #     cursorLocn = pygame.mouse.get_pos()
-    #     if event.type == pygame.MOUSEBUTTONDOWN:
-    #         cont = False
-    #     if event.type == pygame.QUIT:
-    #         cont = False
-            
-    if state == STATE_NOT_OVER:
-        ''' we don't need different settings for different devices -- 
-            They are all just FSRs connected to amplifiers. just make sure
-            the offset and gain are appropriate on the board. '''
-        # get the current forces for each device
-        start_time = time.time()
-        if mainTarget.isOver(cursorLocn):
-            state = STATE_OVER
-            
-    if state == STATE_OVER:
-        if mainTarget.isOver(cursorLocn):
-            current_time = time.time()
-            timer = current_time-start_time
-            if timer > targetHoldTime:
-                state = STATE_READY_DISPENSE
-        else:
-                state = STATE_NOT_OVER
-
+    elif state = STATE_REWARD:
         
-    if state == STATE_READY_DISPENSE:
-         if buttonReward.is_pressed():
-            rewardSound.play() # beep!
-            rewardControl.on() # turn on the solenoid
-            time.sleep(dispenseTime) # dispense for x seconds
-            rewardControl.off() # turn off solenoid
-            state = STATE_BETWEEN_TRIAL
-            
-    if state == STATE_BETWEEN_TRIAL:
-        time.sleep(interTrial)
-        #change the target parameters
-        state = STATE_NOT_OVER
+        rewardSound.play()
+        
+        
+    elif state = STATE_BETWEEN_TRIALS:
+        blank_screen(screen)
+        sleep(interTrialTime.current)
+        interTrialTime.reroll()
+        targetHoldTime.reroll()
+        dispenseTime.reroll()
+        dev,tgt = restart_task(devDict,tgtDict)
+        goSound.play()
+
